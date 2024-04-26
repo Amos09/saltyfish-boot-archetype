8c98deeadc73ab34138d83f73bb7235c4d5e9cad
zhangminhua <koali09@163.com>
2024-04-26 17:49:00 +0800

    调整代码

--- saltyfish-boot-server/src/main/java/com/saltyfish/ServerStartApplication.java
+++ saltyfish-boot-server/src/main/java/com/saltyfish/ServerStartApplication.java
@@ -9,20 +9,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 /**
  * @author: 番薯(Amos)
  * @dateTime: 9:51/25:07:2022
- * @version: v1.0
  * @description: 服务启动入口
  */
 @SpringBootApplication
 public class ServerStartApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(ServerStartApplication.class);
-    
-    public static void main(String[] args) {
-        try {
-            ConfigurableApplicationContext run = SpringApplication.run(ServerStartApplication.class, args);
-            logger.info(run.getEnvironment().getProperty("spring.application.name") + " 服务启动成功; 端口号: " + run.getEnvironment().getProperty("server.port"));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+	private static final Logger logger = LoggerFactory.getLogger(ServerStartApplication.class);
+
+	public static void main(String[] args) {
+		try {
+			ConfigurableApplicationContext run = SpringApplication.run(ServerStartApplication.class, args);
+			logger.info(run.getEnvironment().getProperty("spring.application.name") + " 服务启动成功; 端口号: "
+					+ run.getEnvironment().getProperty("server.port"));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
